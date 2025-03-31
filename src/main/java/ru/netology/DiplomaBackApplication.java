067fa1070547ae6229521c9856394c7ce501f9be
Maxim Zharinoff <minimum.cold1991@gmail.com>
2025-03-31 15:54:36 +0700

    init commit

---
+++ src/main/java/ru/netology/DiplomaBackApplication.java
@@ -0,0 +1,12 @@
+package ru.netology;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiplomaBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiplomaBackApplication.class, args);
+    }
+}
