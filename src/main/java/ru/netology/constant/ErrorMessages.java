0772cad2de079ca035fa6ed6d7aa7b3723359f1f
Maxim Zharinoff <minimum.cold1991@gmail.com>
2025-04-07 17:13:17 +0700

    create enums: ErrorMessages and SqlQueries

---
+++ src/main/java/ru/netology/constant/ErrorMessages.java
@@ -0,0 +1,20 @@
+package ru.netology.constant;
+
+public enum ErrorMessages {
+    // 400
+    ERR_LOGIN("Bad credentials"),
+    ERR_INPUT("Error input data"),
+
+    // 401
+    ERR_TOKEN("Unauthorized error"),
+
+    // 500
+    ERR_DELETE("Error delete file"),
+    ERR_UPLOAD("Error upload file");
+
+    public final String message;
+
+    ErrorMessages(String message) {
+        this.message = message;
+    }
+}
---
+++ src/main/java/ru/netology/constant/SqlQueries.java
@@ -0,0 +1,35 @@
+package ru.netology.constant;
+
+public enum SqlQueries {
+
+    //auth
+    CHECK_CREDENTIALS("SELECT COUNT(*) " +
+            "FROM netology.users WHERE login = ? AND password = ?"),
+
+    //token
+    CREATE_TOKEN("INSERT INTO netology.auth_tokens " +
+            "(user_name, token) VALUES (?, ?)"),
+    CHECK_TOKEN("SELECT COUNT(*) " +
+            "FROM netology.auth_tokens WHERE token = ?"),
+    DROP_TOKEN("DELETE FROM netology.auth_tokens " +
+            "WHERE token = ?"),
+
+    //file
+    GET_FILE("SELECT file_data, filename " +
+            "FROM netology.files " +
+            "WHERE user_name = ? AND filename = ?"),
+    SAVE_FILE("INSERT INTO netology.files " +
+            "(user_name, filename, file_data, hash, size) " +
+            "VALUES (?, ?, ?, ?, ?)"),
+    DELETE_FILE("DELETE FROM netology.files " +
+            "WHERE user_name = ? AND filename = ?"),
+    GET_FILE_LIST("SELECT filename, size " +
+            "FROM netology.files " +
+            "WHERE user_name = ? ORDER BY filename LIMIT ?");
+
+    public final String query;
+
+    SqlQueries(String query) {
+        this.query = query;
+    }
+}
