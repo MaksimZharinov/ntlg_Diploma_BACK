0594b48b8b7e4bec5aab0c2bf528419a1fdb361a
Maxim Zharinoff <minimum.cold1991@gmail.com>
2025-04-07 15:07:26 +0700

    create dto for Login

---
+++ src/main/java/ru/netology/dto/LoginRequest.java
@@ -0,0 +1,9 @@
+package ru.netology.dto;
+
+import lombok.Data;
+
+@Data
+public class LoginRequest {
+    private String login;
+    private String password;
+}
---
+++ src/main/java/ru/netology/dto/LoginResponse.java
@@ -0,0 +1,13 @@
+package ru.netology.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class LoginResponse {
+
+    @JsonProperty("auth-token")
+    private String authToken;
+}
